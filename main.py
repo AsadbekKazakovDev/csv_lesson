69596d4b9bde434c7e104d66ba1af5d14c7d27a7
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-11 10:18:30 +0500

    solved main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+from pprint import pprint
 def get_country_column(file_name):
     """
     This function takes a filename as input and returns a list of countries
@@ -6,4 +8,22 @@ def get_country_column(file_name):
     Returns:
         list
     """
-    return 0
+    #countries = []
+
+    file = open(file_name,"r")
+    reader = csv.reader(file)
+    countries = []
+        # Process each row
+    for row in list(reader)[1:]:
+            # Extract the country from the row
+        country = row[3]
+
+            # Add the country to the list if it's not already present
+        if country not in countries:
+            countries.append(country)
+
+    return countries
+
+file_name = 'data.csv'
+countries = get_country_column(file_name)
+print(countries)
